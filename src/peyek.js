c6ffafea941512cb70895b77a378796dd855ff30
faqih15 <jahidiyam1592@gmail.com>
2021-08-02 21:47:34 +0700

    tambahkalkulator

---
+++ src/peyek.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class peyek extends Component {
+    state = {
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
